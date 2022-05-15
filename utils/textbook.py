8ade4cc67d3c75d6e9250fcd28e5ef9bb692beb3
Miha Torkar <mihatorkar@outlook.com>
2022-05-15 11:48:27 +0200

    Small change in textbook.py

--- utils/textbook.py
+++ utils/textbook.py
@@ -37,7 +37,7 @@ def convert_pdf_to_text(textbook_path):
     with open(textbook_path, "rb") as f:
         pdf = pdftotext.PDF(f)
     print(pdf)
-    return "".join(list(pdf))
+    return "".join(list(pdf)[10:])
 
 if __name__ == "__main__":
     print(convert_pdf_to_text(""))
